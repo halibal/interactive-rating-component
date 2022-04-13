637538f822edd156076d8753c03f6509f16e7a87
Halibal <halibal95@gmail.com>
2022-04-13 17:34:50 +0300

    still working on buttons hover state

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function makeGrey(x) {
-    x.style.backgroundColor = 'hsl(217, 12%, 63%)';
+function makeOrange(x) {
+    x.style.backgroundColor = 'rgb(252, 124, 5)';
 }
 
 function backToNormal(x) {
     x.style.backgroundColor = 'hsl(215, 17%, 22%)';
+}
+
+function submitHover(x) {
+    x.style.backgroundColor = 'hsl(0, 0%, 100%)';
+    x.style.color = 'red';
+}
+
+function submitNormal(x) {
+    x.style.backgroundColor = 'rgb(252, 124, 5)';
+    x.style.color = 'white';
+  }
+
+function btnSelected(x) {
+    x.style.backgroundColor = 'hsl(217, 12%, 63%)';
+
 }
