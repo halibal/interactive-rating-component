03aa1668b62859aa756a4c7d3bb14c5a9cef0d5e
Halibal <halibal95@gmail.com>
2022-04-13 13:42:49 +0300

    hover effect added to evaluation buttons

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-let ptButton = document.getElementsByTagName("button")
-console.log(ptButton)
+function makeGrey(x) {
+    x.style.backgroundColor = 'hsl(217, 12%, 63%)';
+}
+
+function backToNormal(x) {
+    x.style.backgroundColor = 'hsl(215, 17%, 22%)';
+}
