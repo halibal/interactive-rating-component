c7455adac09c8cd88b110b0f1cc17b81cc8bae72
Canan <halibal95@gmail.com>
2022-04-14 15:01:58 +0300

    submit button displays greeting page

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const ratedNum = document.getElementsByClassName("ratedNum")
+const smallContainer1 = document.querySelector(".smallContainer1")
+const smallContainer2 = document.querySelector(".smallContainer2")
+const submitPoint = document.querySelector("#submitPoint")
+let greetingContainer = document.getElementById("greetingContainer")
+
 function makeOrange(x) {
     x.style.backgroundColor = 'rgb(252, 124, 5)';
 }
@@ -6,17 +12,25 @@ function backToNormal(x) {
     x.style.backgroundColor = 'hsl(215, 17%, 22%)';
 }
 
-function submitHover(x) {
-    x.style.backgroundColor = 'hsl(0, 0%, 100%)';
-    x.style.color = 'red';
-}
+// function submitHover(x) {
+//     x.style.backgroundColor = 'hsl(0, 0%, 100%)';
+//     x.style.
+// }
 
-function submitNormal(x) {
-    x.style.backgroundColor = 'rgb(252, 124, 5)';
-    x.style.color = 'white';
-  }
+// function submitNormal(x) {
+//     x.style.backgroundColor = 'rgb(252, 124, 5)';
+//   }
 
 function btnSelected(x) {
     x.style.backgroundColor = 'hsl(217, 12%, 63%)';
 
+}
+
+function greetingNum() {
+    ratedNum.innerHTML = `You selected ${rating} out of 5`
+}
+
+function greetingDisplay() {
+    greetingContainer.classList.remove("d-none");
+    smallContainer1.classList.add("d-none")
 }
