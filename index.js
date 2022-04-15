348befbdda6951e1111b8980a2d9d50d5051e660
Canan <halibal95@gmail.com>
2022-04-15 12:19:45 +0300

    working on btn-points

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const ratedNum = document.getElementsByClassName("ratedNum")
 const smallContainer1 = document.querySelector(".smallContainer1")
 const smallContainer2 = document.querySelector(".smallContainer2")
 const submitPoint = document.querySelector("#submitPoint")
-let greetingContainer = document.getElementById("greetingContainer")
+const greetingContainer = document.getElementById("greetingContainer")
+const containerCircle = document.getElementsByClassName("containerCircle")
+const points = document.getElementsByClassName("points")
+// function makeOrange(x) {
+//     x.style.backgroundColor = 'rgb(252, 124, 5)';
+// }
 
-function makeOrange(x) {
-    x.style.backgroundColor = 'rgb(252, 124, 5)';
-}
-
-function backToNormal(x) {
-    x.style.backgroundColor = 'hsl(215, 17%, 22%)';
-}
+// function backToNormal(x) {
+//     x.style.backgroundColor = 'hsl(215, 17%, 22%)';
+// }
 
 // function submitHover(x) {
 //     x.style.backgroundColor = 'hsl(0, 0%, 100%)';
@@ -21,10 +22,22 @@ function backToNormal(x) {
 //     x.style.backgroundColor = 'rgb(252, 124, 5)';
 //   }
 
+// for (let i = 0; i < points.length; i++) {
+//     points[i].addEventListener("click", function() {
+//         let selectedBtn = document.querySelector(".selectedBtn");
+//         if (selectedBtn) {
+//             selectedBtn.classList.remove("selectedBtn");
+//         }
+//         this.classList.add("selectedBtn");
+//     }, false)
+// }
+
+
 function btnSelected(x) {
     x.style.backgroundColor = 'hsl(217, 12%, 63%)';
-
 }
+
+
 
 function greetingNum() {
     ratedNum.innerHTML = `You selected ${rating} out of 5`
@@ -34,3 +47,9 @@ function greetingDisplay() {
     greetingContainer.classList.remove("d-none");
     smallContainer1.classList.add("d-none")
 }
+
+points.array.forEach((point) => {
+    point.addEventListener("click", ()=>{
+        point.classList.toggle("selectedBtn");
+    });
+});
